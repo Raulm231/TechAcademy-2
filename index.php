1066849f48a8c4547cd7345f705c084e3a10f210
DudaMonesi <161781907+DudaMonesi@users.noreply.github.com>
2024-06-03 22:08:27 -0300

    Adicionei SASS ao site

--- index.php
+++ index.php
@@ -3,18 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/css.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <title>PHOENIX</title>
 </head>
 <body>
     <header>
-        
+        <a href="">
+            <img src="imagens/_5926fa45-9bda-4fb7-932b-49650c112174 (1).png" width="100px" height="100px" alt="">
+        </a>
+        <nav class="header-menu">
+            <ul>
+                <li>
+                    <a href="" title="Inicio">Inicio</a>
+                </li>
+                <li>
+                    <a href="" title="Contato">Contato</a>
+                </li>
+                <li>
+                    <a href="" title="Sobre">Sobre</a>
+                </li>
+            </ul>
+        <nav>
     </header>
     <main>
-
+        <php?
+        ?php>
     </main>
-    <footer>
+    <br>
+    <section id="principal">
+    <h1>Seja Bem Vindo ao PHOENIX</h1>
+    <br>
+    <p>Aqui é o lugar onde você pode encontrar os melhores jogos:</p>
+    <section>
+        <div class="Imagens">
+            <img src="" alt="">
+        <div>
 
+    <footer>
+        <h3>site desenvolvido por Raul Miguel<h3>
     </footer>
 </body>
 </html>

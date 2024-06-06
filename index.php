863962986674425b74c7ff14c3deb3f54f384b31
Raulm231 <sraulmiguel231@gmail.com>
2024-06-06 15:18:12 -0300

    Adicionei Bootstrap ao Site

--- index.php
+++ index.php
@@ -4,32 +4,52 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.css">
+    <link rel="stylesheet" href="css/bootstrap-reboot.css">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    
     <title>PHOENIX</title>
 </head>
 <body>
-    <header>
-        <a href="">
-            <img src="imagens/_5926fa45-9bda-4fb7-932b-49650c112174 (1).png" width="100px" height="100px" alt="">
-        </a>
-        <nav class="header-menu">
-            <ul>
-                <li>
-                    <a href="" title="Inicio">Inicio</a>
-                </li>
-                <li>
-                    <a href="" title="Contato">Contato</a>
-                </li>
-                <li>
-                    <a href="" title="Sobre">Sobre</a>
-                </li>
-            </ul>
-        <nav>
-    </header>
+<nav class="navbar navbar-expand-lg">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+        <img src="imagens/_5926fa45-9bda-4fb7-932b-49650c112174 (1).png">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="quemsomos.php">Quem Somos</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="games.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            GAMES
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Desordeon</a></li>
+            <li><a class="dropdown-item" href="#">Mario World</a></li>
+            <li><a class="dropdown-item" href="#">Pokémon</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     <main>
         <php?
+
+
         ?php>
     </main>
     <br>
@@ -43,7 +63,7 @@
         <div>
 
     <footer>
-        <h3>site desenvolvido por Raul Miguel<h3>
+        <h3>Site desenvolvido por Raul Miguel<h3>
     </footer>
 </body>
 </html>

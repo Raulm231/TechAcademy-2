2ce7ebd888a41c6e8aa264af81ff3b4a1a71fbd0
unknown <Administrador@gi02tads56.integrado.int>
2024-06-17 22:27:31 -0300

    Correção do carrosel, implementação de API, personalização do scroll bar, Botão de contato do Header

--- paginas/home.php
+++ paginas/home.php
@@ -1,28 +1,33 @@
 <div class="pghome">
 
-<br>
+    <br>
 
     <h2>RECOMENDAÇÕES EM DESTAQUE:</h2>
 
     <br>
 
-    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+    <div id="carouselExampleIndicators" class="carousel slide">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="imagens/red dead 3.webp" class="d-block w-100" alt="Red Dead Redemption 45" height="600px">
+                <img src="imagens/Des-Banner.png" class="d-block w-100" alt="Desordeon">
             </div>
             <div class="carousel-item">
-                <img src="imagens/red dead 3.webp" class="d-block w-100" alt="Barbie" height="600px">
+                <img src="imagens/red dead 3.webp" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="imagens/red dead 3.webp" class="d-block w-100" alt="Sea" height="600px">
+                <img src="..." class="d-block w-100" alt="...">
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
@@ -34,7 +39,12 @@
 
     <br>
 
-    <p>fefeaf</p>
+    <div class="card" style="width: 18rem;">
+        <img src="imagens/repeido/cc-vertical.jpg" class="card-img-top" alt="..." height="250px">
+        <div class="card-body">
+            <p class="card-text">Crimson Curse</p>
+        </div>
+    </div>
 
 
 
